ce53a8b7a8879f196cf30328a71ab2d04aee73a6
Abhriya <abhriya.roy@biofourmis.com>
2023-05-13 11:18:27 +0530

    Add load and delete methods to viewmodel

---
+++ kmm/ClipR/src/main/java/studio/zebro/clipr/android/copieditemslist/CopiedItemsListState.kt
@@ -0,0 +1,9 @@
+package studio.zebro.clipr.android.copieditemslist
+
+import studio.zebro.clipr.domain.copieditems.CopiedItem
+
+data class CopiedItemsListState(
+  val copiedItemsList : List<CopiedItem> = emptyList(),
+  val searchText : String = "",
+  val isSearchActive : Boolean = false
+)
