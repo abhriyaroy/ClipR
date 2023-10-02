a87855e75ceaad716e804e6e30bc2bc4c5e6b0a1
Abhriya <abhriya.roy@biofourmis.com>
2023-10-02 16:50:30 +0530

    Splitup login screen into functions

--- kmm/cliprAndroid/src/main/java/studio/zebro/clipr/android/presentation/screen/authentication/login/LoginScreen.kt
+++ kmm/cliprAndroid/src/main/java/studio/zebro/clipr/android/presentation/screen/authentication/login/LoginScreen.kt
@@ -13,13 +13,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 import org.koin.androidx.compose.getViewModel
 import studio.zebro.clipr.android.R
 import studio.zebro.clipr.android.presentation.navigation.AppNavigationRoutes
@@ -28,17 +25,36 @@ import studio.zebro.clipr.android.presentation.widgets.ButtonWithLoader
 import studio.zebro.clipr.android.presentation.widgets.RoundedInputText
 import studio.zebro.clipr.ui.theming.Colors
 
-private var loginScreenTitleSlideOut = mutableStateOf(false)
-private var loginScreenUsernameSlideOut = mutableStateOf(false)
-private var loginScreenPasswordSlideOut = mutableStateOf(false)
-private var loginScreenLoginButtonSlideOut = mutableStateOf(false)
-private var loginScreenSignupButtonSlideOut = mutableStateOf(false)
-
 @Composable
 fun LoginScreen(navHostController: NavHostController) {
 
   val loginViewModel: LoginViewModel = getViewModel()
   val viewState by loginViewModel.viewState.collectAsState()
+
+  Box(modifier = Modifier.fillMaxSize()) {
+    LoginScreenFields(
+      navHostController = navHostController,
+      loginViewModel = loginViewModel,
+      viewState = viewState,
+      modifier = Modifier.align(Alignment.CenterStart)
+    )
+  }
+
+  DisposableEffect(Unit) {
+    onDispose {
+      loginViewModel.notifyViewRemoved()
+    }
+  }
+
+}
+
+@Composable
+private fun LoginScreenFields(
+  navHostController: NavHostController,
+  loginViewModel: LoginViewModel,
+  viewState: LoginViewState,
+  modifier: Modifier
+) {
 
   val inputUserName = remember {
     mutableStateOf("")
@@ -50,10 +66,14 @@ fun LoginScreen(navHostController: NavHostController) {
   val shouldEnableLoginButton = remember { mutableStateOf(false) }
   val isLoading = remember { mutableStateOf(false) }
 
-  val isPasswordHidden = remember { mutableStateOf(true) }
-
   val slideOutOffset = (-500).dp
   val originalOffset = 0.dp
+
+  val loginScreenTitleSlideOut = remember { mutableStateOf(false) }
+  val loginScreenUsernameSlideOut = remember { mutableStateOf(false) }
+  val loginScreenPasswordSlideOut = remember { mutableStateOf(false) }
+  val loginScreenLoginButtonSlideOut = remember { mutableStateOf(false) }
+  val loginScreenSignupButtonSlideOut = remember { mutableStateOf(false) }
 
   val titleTransition = updateTransition(loginScreenTitleSlideOut.value, null)
   val userNameTransition = updateTransition(loginScreenUsernameSlideOut.value, null)
@@ -64,159 +84,142 @@ fun LoginScreen(navHostController: NavHostController) {
   val titleTextFieldOffset by titleTransition.animateDp(label = "") { state ->
     if (state) slideOutOffset else originalOffset
   }
-
   val usernameTextFieldOffset by userNameTransition.animateDp(label = "") { state ->
     if (state) slideOutOffset else originalOffset
   }
-
   val passwordTextFieldOffset by passwordTransition.animateDp(label = "") { state ->
     if (state) slideOutOffset else originalOffset
   }
-
   val loginButtonOffset by loginTransition.animateDp(label = "") { state ->
     if (state) slideOutOffset else originalOffset
   }
-
   val signupButtonOffset by signupTransition.animateDp(label = "") { state ->
     if (state) slideOutOffset else originalOffset
   }
 
-
-  Box(modifier = Modifier.fillMaxSize()) {
-    Column(
+  Column(
+    modifier = modifier
+      .padding(16.dp)
+  ) {
+    Text(
+      text = stringResource(id = R.string.app_name),
       modifier = Modifier
-        .align(Alignment.CenterStart)
-        .padding(16.dp)
-    ) {
-      Text(
-        text = stringResource(id = R.string.app_name),
-        modifier = Modifier
-          .fillMaxWidth()
-          .offset(titleTextFieldOffset),
-        style = MaterialTheme.typography.displayLarge,
-        color = Colors.white100
+        .fillMaxWidth()
+        .offset(titleTextFieldOffset),
+      style = MaterialTheme.typography.displayLarge,
+      color = Colors.white100
+    )
+    RoundedInputText(
+      modifier = Modifier.offset(usernameTextFieldOffset),
+      initialValue = inputUserName,
+      hint = stringResource(id = R.string.username_hint),
+      leadingImage = Icons.Default.Person,
+      maxLines = 1,
+      onTextChanged = {
+        loginViewModel.handleUserNameInput(it)
+      },
+    )
+    Spacer(modifier = Modifier.height(8.dp))
+    PasswordField(loginViewModel, passwordTextFieldOffset, inputPassword)
+    Spacer(modifier = Modifier.height(16.dp))
+    Box(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+      ButtonWithLoader(
+        modifier = Modifier.offset(loginButtonOffset),
+        isLoading.value,
+        shouldEnableLoginButton.value,
+        text = stringResource(id = R.string.login),
+        onClick = loginViewModel::handleLoginClick
       )
-      RoundedInputText(
-        modifier = Modifier.offset(usernameTextFieldOffset),
-        initialValue = inputUserName,
-        hint = stringResource(id = R.string.username_hint),
-        leadingImage = Icons.Default.Person,
-        maxLines = 1,
-        onTextChanged = {
-          loginViewModel.handleUserNameInput(it)
-        },
+    }
+    Spacer(modifier = Modifier.height(8.dp))
+    Box(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+      ButtonWithLoader(
+        modifier = Modifier.offset(signupButtonOffset),
+        isLoading = false,
+        isEnabled = true,
+        text = stringResource(id = R.string.signup),
+        onClick = loginViewModel::handleSignUpClickInLoginScreen
       )
-      Spacer(modifier = Modifier.height(8.dp))
-      RoundedInputText(
-        modifier = Modifier.offset(passwordTextFieldOffset),
-        initialValue = inputPassword,
-        hint = stringResource(id = R.string.password_hint),
-        leadingImage = ImageVector.vectorResource(id = R.drawable.lock),
-        maxLines = 1,
-        onTextChanged = {
-          loginViewModel.handlePasswordInput(it)
-        },
-        isPasswordField = true,
-        shouldHideInput = isPasswordHidden.value,
-        onHideToggleClick = {
-          isPasswordHidden.value = !isPasswordHidden.value
-        }
-      )
-      Spacer(modifier = Modifier.height(16.dp))
-      Box(modifier = Modifier.align(Alignment.CenterHorizontally)) {
-        ButtonWithLoader(
-          modifier = Modifier.offset(loginButtonOffset),
-          isLoading.value,
-          shouldEnableLoginButton.value,
-          text = stringResource(id = R.string.login),
-          onClick = loginViewModel::handleLoginClick
-        )
-      }
-      Spacer(modifier = Modifier.height(8.dp))
-      Box(modifier = Modifier.align(Alignment.CenterHorizontally)) {
-        ButtonWithLoader(
-          modifier = Modifier.offset(signupButtonOffset),
-          isLoading = false,
-          isEnabled = true,
-          text = stringResource(id = R.string.signup),
-          onClick = loginViewModel::handleSignUpClickInLoginScreen
-        )
-      }
     }
   }
 
   LaunchedEffect(viewState) {
     when (viewState) {
       is LoginViewState.EnterNavigation -> {
-        reenterLoginScreenWithDelay()
+        delay(400)
+        loginScreenTitleSlideOut.value = false
+        delay(50)
+        loginScreenUsernameSlideOut.value = false
+        delay(50)
+        loginScreenPasswordSlideOut.value = false
+        delay(50)
+        loginScreenLoginButtonSlideOut.value = false
+        delay(50)
+        loginScreenSignupButtonSlideOut.value = false
       }
+
       is LoginViewState.SignUpNavigation -> {
-        exitLoginScreenToSignupScreen(navHostController)
+        loginScreenTitleSlideOut.value = true
+        delay(50)
+        loginScreenUsernameSlideOut.value = true
+        delay(50)
+        loginScreenPasswordSlideOut.value = true
+        delay(50)
+        loginScreenLoginButtonSlideOut.value = true
+        delay(50)
+        loginScreenSignupButtonSlideOut.value = true
+        navHostController.navigate(AppNavigationRoutes.SIGNUP_SCREEN)
       }
+
       is LoginViewState.Empty -> {
         loginViewModel.notifyViewCreated()
       }
+
       is LoginViewState.InputValidation -> {
-        with(viewState as LoginViewState.InputValidation) {
-          if (inputUserName.value != userName) {
-            inputUserName.value = userName
-          }
-          if (inputPassword.value != password) {
-            inputPassword.value = password
-          }
-          shouldEnableLoginButton.value = isValid
+        if (inputUserName.value != viewState.userName) {
+          inputUserName.value = viewState.userName
         }
+        if (inputPassword.value != viewState.password) {
+          inputPassword.value = viewState.password
+        }
+        shouldEnableLoginButton.value = viewState.isValid
       }
+
       is LoginViewState.LoginSuccess -> {
-        with(viewState as LoginViewState.LoginSuccess) {
-          isLoading.value = false
-        }
+        isLoading.value = false
+
       }
+
       is LoginViewState.LoginError -> {
 
       }
+
       else -> {}
     }
   }
+}
 
-  DisposableEffect(Unit) {
-    onDispose {
-      loginViewModel.notifyViewRemoved()
+@Composable
+private fun PasswordField(
+  loginViewModel: LoginViewModel,
+  passwordTextFieldOffset: Dp,
+  inputPassword: MutableState<String>
+) {
+  val isPasswordHidden = remember { mutableStateOf(true) }
+
+  RoundedInputText(
+    modifier = Modifier.offset(passwordTextFieldOffset),
+    initialValue = inputPassword,
+    hint = stringResource(id = R.string.password_hint),
+    leadingImage = ImageVector.vectorResource(id = R.drawable.lock),
+    maxLines = 1,
+    onTextChanged = {
+      loginViewModel.handlePasswordInput(it)
+    },
+    isPasswordField = true,
+    shouldHideInput = isPasswordHidden.value,
+    onHideToggleClick = {
+      isPasswordHidden.value = !isPasswordHidden.value
     }
-  }
-
-}
-
-private fun reenterLoginScreenWithDelay() {
-  CoroutineScope(Dispatchers.IO).launch {
-    delay(400)
-    loginScreenTitleSlideOut.value = false
-    delay(50)
-    loginScreenUsernameSlideOut.value = false
-    delay(50)
-    loginScreenPasswordSlideOut.value = false
-    delay(50)
-    loginScreenLoginButtonSlideOut.value = false
-    delay(50)
-    loginScreenSignupButtonSlideOut.value = false
-  }
-}
-
-private fun exitLoginScreenToSignupScreen(navHostController: NavHostController) {
-  CoroutineScope(Dispatchers.Main).launch {
-    exitLoginScreen()
-    navHostController.navigate(AppNavigationRoutes.SIGNUP_SCREEN)
-  }
-}
-
-private suspend fun exitLoginScreen() {
-  loginScreenTitleSlideOut.value = true
-  delay(50)
-  loginScreenUsernameSlideOut.value = true
-  delay(50)
-  loginScreenPasswordSlideOut.value = true
-  delay(50)
-  loginScreenLoginButtonSlideOut.value = true
-  delay(50)
-  loginScreenSignupButtonSlideOut.value = true
+  )
 }
